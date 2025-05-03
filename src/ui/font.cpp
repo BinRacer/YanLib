b1f4742598e020743197d4d2e8af9b896a7207de
BinRacer <native.lab@outlook.com>
2025-05-03 13:33:22 +0800

    add ui/font files

---
+++ src/ui/font.cpp
@@ -0,0 +1,130 @@
+//
+// Created by forkernel on 2025/5/3.
+//
+
+#include "font.h"
+
+namespace YanLib::ui {
+    HFONT font::create_font(const wchar_t *face_name,
+                            DWORD charset,
+                            int height,
+                            int width,
+                            int escapement,
+                            int orientation,
+                            int weight,
+                            bool is_italic,
+                            bool is_underline,
+                            bool is_strike_out,
+                            DWORD out_precision,
+                            DWORD clip_precision,
+                            DWORD quality,
+                            DWORD pitch_and_family) {
+        return CreateFontW(height,
+                           width,
+                           escapement,
+                           orientation,
+                           weight,
+                           is_italic ? TRUE : FALSE,
+                           is_underline ? TRUE : FALSE,
+                           is_strike_out ? TRUE : FALSE,
+                           charset,
+                           out_precision,
+                           clip_precision,
+                           quality,
+                           pitch_and_family,
+                           face_name);
+    }
+
+    HFONT font::create_font_indirect(const LOGFONTW *log_font) {
+        return CreateFontIndirectW(log_font);
+    }
+
+    LOGFONTW font::make_log_font(const wchar_t *face_name,
+                                 uint8_t char_set,
+                                 LONG height,
+                                 LONG width,
+                                 LONG escapement,
+                                 LONG orientation,
+                                 LONG weight,
+                                 bool is_italic,
+                                 bool is_underline,
+                                 bool is_strike_out,
+                                 uint8_t out_precision,
+                                 uint8_t clip_precision,
+                                 uint8_t quality,
+                                 uint8_t pitch_and_family) {
+        LOGFONTW log_font = {};
+        log_font.lfHeight = height;
+        log_font.lfWidth = width;
+        log_font.lfEscapement = escapement;
+        log_font.lfOrientation = orientation;
+        log_font.lfWeight = weight;
+        log_font.lfItalic = is_italic ? TRUE : FALSE;
+        log_font.lfUnderline = is_underline ? TRUE : FALSE;
+        log_font.lfStrikeOut = is_strike_out ? TRUE : FALSE;
+        log_font.lfOutPrecision = out_precision;
+        log_font.lfClipPrecision = clip_precision;
+        log_font.lfQuality = quality;
+        log_font.lfPitchAndFamily = pitch_and_family;
+        wcscpy_s(log_font.lfFaceName, LF_FACESIZE, face_name);
+        return log_font;
+    }
+
+    HFONT font::create_font_indirect(const ENUMLOGFONTEXDVW *enum_log_font) {
+        return CreateFontIndirectExW(enum_log_font);
+    }
+
+    HANDLE font::add_font_mem_resource(void *file_view,
+                                       DWORD size,
+                                       void *resrved,
+                                       DWORD *num_fonts) {
+        return AddFontMemResourceEx(file_view, size, resrved, num_fonts);
+    }
+
+    bool font::remove_font_mem_resource(HANDLE font_res_handle) {
+        return RemoveFontMemResourceEx(font_res_handle);
+    }
+
+    int font::add_font_resource(const wchar_t *font_file_name) {
+        return AddFontResourceW(font_file_name);
+    }
+
+    bool font::remove_font_resource(const wchar_t *font_file_name) {
+        return RemoveFontResourceW(font_file_name);
+    }
+
+    int font::add_font_resource(const wchar_t *font_file_name, DWORD font_type) {
+        return AddFontResourceExW(font_file_name, font_type, nullptr);
+    }
+
+    int font::remove_font_resource(const wchar_t *font_file_name, DWORD font_type) {
+        return RemoveFontResourceExW(font_file_name, font_type, nullptr);
+    }
+
+    int font::enum_font_families(HDC dc_handle,
+                                 LPLOGFONTW log_font,
+                                 FONTENUMPROCW font_enum_proc,
+                                 LPARAM l_param) {
+        return EnumFontFamiliesExW(dc_handle,
+                                   log_font,
+                                   font_enum_proc,
+                                   l_param,
+                                   0);
+    }
+
+    DWORD font::get_font_data(HDC dc_handle,
+                              DWORD table,
+                              DWORD offset,
+                              void *buf,
+                              DWORD size) {
+        return GetFontData(dc_handle, table, offset, buf, size);
+    }
+
+    DWORD font::get_font_language_info(HDC dc_handle) {
+        return GetFontLanguageInfo(dc_handle);
+    }
+
+    DWORD font::get_font_unicode_ranges(HDC dc_handle, LPGLYPHSET glyph_set) {
+        return GetFontUnicodeRanges(dc_handle, glyph_set);
+    }
+}
---
+++ src/ui/font.h
@@ -0,0 +1,91 @@
+//
+// Created by forkernel on 2025/5/3.
+//
+
+#ifndef FONT_H
+#define FONT_H
+#include <cstdint>
+#include <Windows.h>
+
+namespace YanLib::ui {
+    class font {
+    public:
+        font(const font &other) = delete;
+
+        font(font &&other) = delete;
+
+        font &operator=(const font &other) = delete;
+
+        font &operator=(font &&other) = delete;
+
+        font() = default;
+
+        ~font() = default;
+
+        static HFONT create_font(const wchar_t *face_name = L"宋体",
+                                 DWORD charset = GB2312_CHARSET,
+                                 int height = 12,
+                                 int width = 0,
+                                 int escapement = 0,
+                                 int orientation = 0,
+                                 int weight = 0,
+                                 bool is_italic = false,
+                                 bool is_underline = false,
+                                 bool is_strike_out = false,
+                                 DWORD out_precision = 0,
+                                 DWORD clip_precision = 0,
+                                 DWORD quality = 0,
+                                 DWORD pitch_and_family = 0);
+
+        static HFONT create_font_indirect(const LOGFONTW *log_font);
+
+        static LOGFONTW make_log_font(const wchar_t *face_name = L"宋体",
+                                      uint8_t char_set = GB2312_CHARSET,
+                                      LONG height = 12,
+                                      LONG width = 0,
+                                      LONG escapement = 0,
+                                      LONG orientation = 0,
+                                      LONG weight = 0,
+                                      bool is_italic = false,
+                                      bool is_underline = false,
+                                      bool is_strike_out = false,
+                                      uint8_t out_precision = 0,
+                                      uint8_t clip_precision = 0,
+                                      uint8_t quality = 0,
+                                      uint8_t pitch_and_family = 0);
+
+        static HFONT create_font_indirect(const ENUMLOGFONTEXDVW *enum_log_font);
+
+        static HANDLE add_font_mem_resource(void *file_view,
+                                            DWORD size,
+                                            void *resrved,
+                                            DWORD *num_fonts);
+
+        static bool remove_font_mem_resource(HANDLE font_res_handle);
+
+        static int add_font_resource(const wchar_t *font_file_name);
+
+        static bool remove_font_resource(const wchar_t *font_file_name);
+
+        static int add_font_resource(const wchar_t *font_file_name, DWORD font_type);
+
+        static int remove_font_resource(const wchar_t *font_file_name,
+                                        DWORD font_type);
+
+        static int enum_font_families(HDC dc_handle,
+                                      LPLOGFONTW log_font,
+                                      FONTENUMPROCW font_enum_proc,
+                                      LPARAM l_param);
+
+        static DWORD get_font_data(HDC dc_handle,
+                                   DWORD table,
+                                   DWORD offset,
+                                   void *buf,
+                                   DWORD size);
+
+        static DWORD get_font_language_info(HDC dc_handle);
+
+        static DWORD get_font_unicode_ranges(HDC dc_handle, LPGLYPHSET glyph_set);
+    };
+}
+#endif //FONT_H

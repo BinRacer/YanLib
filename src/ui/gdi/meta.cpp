254e3d9c9cc346e6e14d2fdb3dced0f503fbefa0
BinRacer <native.lab@outlook.com>
2025-05-05 12:58:20 +0800

    add ui/gdi/meta files

---
+++ src/ui/gdi/meta.cpp
@@ -0,0 +1,117 @@
+//
+// Created by forkernel on 2025/5/5.
+//
+
+#include "meta.h"
+
+namespace YanLib::ui::gdi {
+    HDC meta::create_enhance_meta_file(HDC dc_handle,
+                                       const wchar_t *file_name,
+                                       const RECT *rect,
+                                       const wchar_t *desc) {
+        return CreateEnhMetaFileW(dc_handle, file_name, rect, desc);
+    }
+
+    HENHMETAFILE meta::close_enhance_meta_file(HDC dc_handle) {
+        return CloseEnhMetaFile(dc_handle);
+    }
+
+    bool meta::delete_enhance_meta_file(HENHMETAFILE enh_meta_file_handle) {
+        return DeleteEnhMetaFile(enh_meta_file_handle);
+    }
+
+    bool meta::enum_enhance_meta_file(HDC dc_handle,
+                                      HENHMETAFILE enh_meta_file_handle,
+                                      ENHMFENUMPROC enh_meta_file_proc,
+                                      void *param,
+                                      const RECT *rect) {
+        return EnumEnhMetaFile(dc_handle,
+                               enh_meta_file_handle,
+                               enh_meta_file_proc,
+                               param,
+                               rect);
+    }
+
+    HENHMETAFILE meta::copy_enhance_meta_file(HENHMETAFILE enh_meta_file_handle,
+                                              const wchar_t *file_name) {
+        return CopyEnhMetaFileW(enh_meta_file_handle, file_name);
+    }
+
+    bool meta::play_enhance_meta_file(HDC dc_handle,
+                                      HENHMETAFILE enh_meta_file_handle,
+                                      const RECT *rect) {
+        return PlayEnhMetaFile(dc_handle, enh_meta_file_handle, rect);
+    }
+
+    bool meta::play_enhance_meta_file_record(HDC dc_handle,
+                                             HANDLETABLE *table_handle,
+                                             const ENHMETARECORD *enh_meta_record,
+                                             UINT table_num) {
+        return PlayEnhMetaFileRecord(dc_handle,
+                                     table_handle,
+                                     enh_meta_record,
+                                     table_num);
+    }
+
+    HENHMETAFILE meta::get_enhance_meta_file(const wchar_t *file_name) {
+        return GetEnhMetaFileW(file_name);
+    }
+
+    UINT meta::get_enhance_meta_file_header(HENHMETAFILE enh_meta_file_handle,
+                                            UINT size,
+                                            ENHMETAHEADER *buf) {
+        return GetEnhMetaFileHeader(enh_meta_file_handle, size, buf);
+    }
+
+    UINT meta::get_enhance_meta_file_description(HENHMETAFILE enh_meta_file_handle,
+                                                 UINT cch_size,
+                                                 wchar_t *description) {
+        return GetEnhMetaFileDescriptionW(enh_meta_file_handle,
+                                          cch_size,
+                                          description);
+    }
+
+    UINT meta::get_enhance_meta_file_palette_entries(
+        HENHMETAFILE enh_meta_file_handle,
+        UINT num,
+        PALETTEENTRY *palette_entries) {
+        return GetEnhMetaFilePaletteEntries(enh_meta_file_handle,
+                                            num,
+                                            palette_entries);
+    }
+
+    UINT meta::get_win_meta_file_bits(HENHMETAFILE enh_meta_file_handle,
+                                      UINT size,
+                                      uint8_t *buf,
+                                      INT map_mode,
+                                      HDC dc_handle) {
+        return GetWinMetaFileBits(enh_meta_file_handle,
+                                  size,
+                                  buf,
+                                  map_mode,
+                                  dc_handle);
+    }
+
+    HENHMETAFILE meta::set_win_meta_file_bits(
+        UINT size,
+        const uint8_t *buf,
+        HDC dc_handle,
+        const METAFILEPICT *meta_file_pict) {
+        return SetWinMetaFileBits(size, buf, dc_handle, meta_file_pict);
+    }
+
+    UINT meta::get_enhance_meta_file_bits(HENHMETAFILE enh_meta_file_handle,
+                                          UINT size,
+                                          uint8_t *buf) {
+        return GetEnhMetaFileBits(enh_meta_file_handle, size, buf);
+    }
+
+    HENHMETAFILE meta::set_enhance_meta_file_bits(UINT size,
+                                                  const uint8_t *buf) {
+        return SetEnhMetaFileBits(size, buf);
+    }
+
+    bool meta::gdi_comment(HDC dc_handle, UINT size, const uint8_t *buf) {
+        return GdiComment(dc_handle, size, buf);
+    }
+}
---
+++ src/ui/gdi/meta.h
@@ -0,0 +1,94 @@
+//
+// Created by forkernel on 2025/5/5.
+//
+
+#ifndef META_H
+#define META_H
+#include <cstdint>
+#include <Windows.h>
+
+namespace YanLib::ui::gdi {
+    class meta {
+    public:
+        meta(const meta &other) = delete;
+
+        meta(meta &&other) = delete;
+
+        meta &operator=(const meta &other) = delete;
+
+        meta &operator=(meta &&other) = delete;
+
+        meta() = default;
+
+        ~meta() = default;
+
+        static HDC create_enhance_meta_file(HDC dc_handle,
+                                            const wchar_t *file_name,
+                                            const RECT *rect,
+                                            const wchar_t *desc);
+
+        static HENHMETAFILE close_enhance_meta_file(HDC dc_handle);
+
+        static bool delete_enhance_meta_file(HENHMETAFILE enh_meta_file_handle);
+
+        static bool enum_enhance_meta_file(HDC dc_handle,
+                                           HENHMETAFILE enh_meta_file_handle,
+                                           ENHMFENUMPROC enh_meta_file_proc,
+                                           void *param,
+                                           const RECT *rect);
+
+        static HENHMETAFILE copy_enhance_meta_file(
+            HENHMETAFILE enh_meta_file_handle,
+            const wchar_t *file_name);
+
+        static bool play_enhance_meta_file(HDC dc_handle,
+                                           HENHMETAFILE enh_meta_file_handle,
+                                           const RECT *rect);
+
+        static bool play_enhance_meta_file_record(
+            HDC dc_handle,
+            HANDLETABLE *table_handle,
+            const ENHMETARECORD *enh_meta_record,
+            UINT table_num);
+
+        static HENHMETAFILE get_enhance_meta_file(const wchar_t *file_name);
+
+        static UINT get_enhance_meta_file_header(HENHMETAFILE enh_meta_file_handle,
+                                                 UINT size,
+                                                 ENHMETAHEADER *buf);
+
+        static UINT get_enhance_meta_file_description(
+            HENHMETAFILE enh_meta_file_handle,
+            UINT cch_size,
+            wchar_t *description);
+
+        static UINT get_enhance_meta_file_palette_entries(
+            HENHMETAFILE enh_meta_file_handle,
+            UINT num,
+            PALETTEENTRY *palette_entries);
+
+        static UINT get_win_meta_file_bits(HENHMETAFILE enh_meta_file_handle,
+                                           UINT size,
+                                           uint8_t *buf,
+                                           INT map_mode,
+                                           HDC dc_handle);
+
+        static HENHMETAFILE set_win_meta_file_bits(
+            UINT size,
+            const uint8_t *buf,
+            HDC dc_handle,
+            const METAFILEPICT *meta_file_pict);
+
+        static UINT get_enhance_meta_file_bits(HENHMETAFILE enh_meta_file_handle,
+                                               UINT size,
+                                               uint8_t *buf);
+
+        static HENHMETAFILE set_enhance_meta_file_bits(UINT size,
+                                                       const uint8_t *buf);
+
+        static bool gdi_comment(HDC dc_handle,
+                                UINT size,
+                                const uint8_t *buf);
+    };
+}
+#endif //META_H
